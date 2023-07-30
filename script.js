6a466df2d937a59e016c71fb996939a43a7af12e
unknown <wilverzxc@gmail.com>
2023-07-30 14:28:18 +0800

    Added hotkeys to the button.

--- script.js
+++ script.js
@@ -10,20 +10,224 @@ const btn7 = document.querySelector('.btn-7');
 const btn8 = document.querySelector('.btn-8');
 const btn9 = document.querySelector('.btn-9');
 const btn0 = document.querySelector('.btn-0');
-const clrBtn = document.querySelector('.clr-btn')
+const clrBtn = document.querySelector('.clear-btn')
 const addBtn = document.querySelector('.add-btn');
-const equalsBtn = document.querySelector('.equals-btn')
 const subtBtn = document.querySelector('.sub-btn');
+const multiplytBtn = document.querySelector('.multiply-btn');
+const divtBtn = document.querySelector('.div-btn');
+const equalsBtn = document.querySelector('.equals-btn')
+const dcmlBtn = document.querySelector('.decimal')
+const delBtn = document.querySelector('.del')
 let storageBank=[];
 
 let firstNumber ='';
 let operator = '';
+let secondNumber=''
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '1') {
+
+        
+        screen.innerText+='1'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '2') {
+
+        
+        screen.innerText+='2'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '3') {
+
+        
+        screen.innerText+='3'
+        storageBank=screen.innerText;
+
+    }
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '4') {
+
+        
+        screen.innerText+='4'
+        storageBank=screen.innerText;
+
+    }
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '5') {
+
+        
+        screen.innerText+='5'
+        storageBank=screen.innerText;
+
+    }
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '6') {
+
+        
+        screen.innerText+='6'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '7') {
+
+        
+        screen.innerText+='7'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '8') {
+
+        
+        screen.innerText+='8'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '0') {
+
+        
+        screen.innerText+='0'
+        storageBank=screen.innerText;
+
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '9') {
+
+        
+        screen.innerText+='9'
+        storageBank=screen.innerText;
+
+    }
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '-') {
+
+        firstNumber = storageBank
+        operator = '-';
+        screen.innerText=''
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '/') {
+
+        firstNumber = storageBank
+        operator = '/';
+        screen.innerText=''
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '+') {
+
+        firstNumber = storageBank
+        operator = '+';
+        screen.innerText=''
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === '*') {
+
+        firstNumber = storageBank
+        operator = '*';
+        screen.innerText=''
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === 'Backspace') {
+
+        screen.innerText=screen.innerText.toString().slice(0,-1)
+    firstNumber=screen.innerText;
+    storageBank=firstNumber;
+    }
+})
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key === 'Enter') {
+
+        
+        if (operator=== '+') {
+            const secondNumber = storageBank;
+            const result = parseFloat(firstNumber)+
+                parseFloat(secondNumber);
+                screen.innerText=''
+            screen.innerText=result.toString();
+    
+            storageBank=result;
+        }
+        
+        else if (operator=== '-') {
+            const secondNumber = storageBank;
+            const result = parseFloat(firstNumber)-
+                parseFloat(secondNumber);
+                screen.innerText=''
+            screen.innerText=result.toString();
+    
+            storageBank=result;
+        }
+    
+        else if (operator=== '*') {
+            const secondNumber = storageBank;
+            const result = parseFloat(firstNumber)*
+                parseFloat(secondNumber);
+                screen.innerText=''
+            screen.innerText=result.toString();
+    
+            storageBank=result;
+        }
+    
+        else if (operator=== '/') {
+            const secondNumber = storageBank;
+            const result = (parseFloat(firstNumber)/
+                parseFloat(secondNumber));
+                screen.innerText=''
+            screen.innerText=result.toString();
+    
+            storageBank=result;
+        }
+
+    }
+})
+dcmlBtn.addEventListener('click',()=>{
+
+  
+    screen.innerText+='.'
+    storageBank=screen.innerText;
+
+
+
+})
 
 btn1.addEventListener('click',()=>{
 
+    
   
         screen.innerText+='1'
         storageBank=screen.innerText;
+        
    
 
     
@@ -83,14 +287,16 @@ btn0.addEventListener('click',()=>{
 })
 
 clrBtn.addEventListener('click', () => {
-   screen.innerText=''
-   resultScreen.innerText=''
+   screen.innerText='';
+   storageBank['']
 })
 
 
 
 addBtn.addEventListener('click', (e)=>{
     firstNumber = storageBank
+    secondNumber = screen.innerText
+   storageBank.push[screen.innerText]
     operator = '+';
     screen.innerText=''
 })
@@ -101,11 +307,23 @@ subtBtn.addEventListener('click', (e)=>{
     screen.innerText=''
 })
 
+multiplytBtn.addEventListener('click', (e)=>{
+    firstNumber = storageBank
+    operator = '*';
+    screen.innerText=''
+})
+
+divtBtn.addEventListener('click', (e)=>{
+    firstNumber = storageBank
+    operator = '/';
+    screen.innerText=''
+})
+
 equalsBtn.addEventListener('click', (e) =>  {
     if (operator=== '+') {
         const secondNumber = storageBank;
-        const result = parseInt(firstNumber)+
-            parseInt(secondNumber);
+        const result = parseFloat(firstNumber)+
+            parseFloat(secondNumber);
             screen.innerText=''
         screen.innerText=result.toString();
 
@@ -114,8 +332,28 @@ equalsBtn.addEventListener('click', (e) =>  {
     
     else if (operator=== '-') {
         const secondNumber = storageBank;
-        const result = parseInt(firstNumber)-
-            parseInt(secondNumber);
+        const result = parseFloat(firstNumber)-
+            parseFloat(secondNumber);
+            screen.innerText=''
+        screen.innerText=result.toString();
+
+        storageBank=result;
+    }
+
+    else if (operator=== '*') {
+        const secondNumber = storageBank;
+        const result = parseFloat(firstNumber)*
+            parseFloat(secondNumber);
+            screen.innerText=''
+        screen.innerText=result.toString();
+
+        storageBank=result;
+    }
+
+    else if (operator=== '/') {
+        const secondNumber = storageBank;
+        const result = (parseFloat(firstNumber)/
+            parseFloat(secondNumber));
             screen.innerText=''
         screen.innerText=result.toString();
 
@@ -123,3 +361,8 @@ equalsBtn.addEventListener('click', (e) =>  {
     }
 
 });
+delBtn.addEventListener('click',(e)=>{
+    screen.innerText=screen.innerText.toString().slice(0,-1)
+    firstNumber=screen.innerText;
+    storageBank=firstNumber;
+})
